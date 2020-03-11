79bf994f95afc8049efb78b9c3491217f521bd50
avikjis27 <avikjis27@gmail.com>
2020-03-11 11:33:36 +0530

    Point to old state bucket

--- remote.tf
+++ remote.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "eks-test-state-bucket"
-    key     = "eks.tfstate-new"
+    key     = "eks.tfstate"
     region  = "us-west-2"
     profile = "sandbox-us-west-2"
   }
